9db892882beb6c140f29330ecc6a0b85751330da
Maxisthemoose <oriontothemax@gmail.com>
2024-05-06 13:43:03 -0700

    Added auth provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import createStore from "react-auth-kit/createStore";
+import AuthProvider from "react-auth-kit";
+
+const store = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === "https:",
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <AuthProvider
+    store={store}
+  >
     <App />
-  </React.StrictMode>
+  </AuthProvider>
 );
