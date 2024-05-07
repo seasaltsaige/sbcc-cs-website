15ba1190544d21534b3cb19e432d08b4b41513f7
seasaltsaige <oriontothemax@gmail.com>
2024-05-07 16:33:10 -0700

    home page template

--- frontend/src/routes/Home/Home.tsx
+++ frontend/src/routes/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
-
+import { ReactComponent as LeftArrow } from "../../svgs/LeftArrow.svg";
+import { ReactComponent as RightArrow } from "../../svgs/RightArrow.svg";
 export function Home() {
 
   return (
@@ -13,9 +14,12 @@ export function Home() {
             <p>In relation with Santa Barbara City College</p>
           </div>
           <div className="scroll-item-container">
+            <LeftArrow className="arrow" />
+            {/* Temporary, will have multiple items, based on like, next event and such */}
             <div className="scroll-item">
-
+              <h1 className="scroll-item-text">Welcome to the Computer Science Club!</h1>
             </div>
+            <RightArrow className="arrow" />
           </div>
         </div>
         <div className="background-image">
