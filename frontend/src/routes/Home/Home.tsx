92a9605195e5fd51949bda20fb29edded1dfefad
seasaltsaige <oriontothemax@gmail.com>
2024-05-07 14:23:58 -0700

    i hate images

--- frontend/src/routes/Home/Home.tsx
+++ frontend/src/routes/Home/Home.tsx
@@ -3,12 +3,25 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 
 export function Home() {
+
   return (
-    <div>
+    <>
       <Navbar />
-      <div>
-        Home Page
+      <div className="home-container">
+        <div className="scroll-panel">
+          <div className="relation">
+            <p>In relation with Santa Barbara City College</p>
+          </div>
+          <div className="scroll-item-container">
+            <div className="scroll-item">
+
+            </div>
+          </div>
+        </div>
+        <div className="background-image">
+          <img src="/FRONT PAGE BACKGROUND.png" alt="Humanities Building" />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
