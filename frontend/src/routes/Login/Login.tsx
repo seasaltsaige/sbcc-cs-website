c8ff3974a09c25bfc9649b69a0b8b1918b7c0720
seasaltsaige <oriontothemax@gmail.com>
2024-05-08 16:28:50 -0700

    Work on login form, will need to implement api connection

--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -28,14 +28,15 @@ export function Login() {
           ["Login", "Register", "Update"].map((val, i) => (
             <button
               key={i}
-              className={`auth-button ${val.toLowerCase() === login_register_update ? "button-active" : ""}`}>
+              className={`auth-button ${val.toLowerCase() === login_register_update ? "button-active" : ""}`}
+              onClick={() => setLogin_Register_Update(val.toLowerCase() as "login" | "register" | "update")}>
               {val}
             </button>
           ))
         }
       </div>
       <div className="auth-panel">
-        <img className="login-logo" src="/file.png" alt="Login Logo" />
+        <img className="login-logo" src="/loginlogo.png" alt="Login Logo" />
         {
           login_register_update === "login" ?
             (
@@ -57,10 +58,47 @@ export function Login() {
             (
               login_register_update === "update" ?
                 (
-                  <></>
+                  <form>
+                    <div>
+                      <label htmlFor="username">Username</label>
+                      <input type="text" placeholder="Enter Username" name="username" required />
+                    </div>
+                    <div>
+                      <label htmlFor="password">Password</label>
+                      <input type="password" placeholder="Enter Password" name="password" required />
+                    </div>
+
+                    <div>
+                      <label style={{marginTop: 10, }} htmlFor="newUsername">New Username</label>
+                      <input type="text" placeholder="Enter New Username (optional)" name="newUsername" />
+                    </div>
+                    <div>
+                      <label htmlFor="newPassword">New Password</label>
+                      <input type="password" placeholder="Enter Password" name="newPassword" required />
+                    </div>
+
+
+                    <button className="submit-auth" type="submit">Update Admin</button>
+                  </form>
                 ) :
                 (
-                  <></>
+                  <form>
+                    <div>
+                      <label htmlFor="username">Username</label>
+                      <input type="text" placeholder="Enter Username" name="username" required />
+                    </div>
+                    <div>
+                      <label htmlFor="password">Password</label>
+                      <input type="password" placeholder="Enter Password" name="password" required />
+                    </div>
+
+                    <div>
+                      <label htmlFor="confirm">Confirm Password</label>
+                      <input type="password" placeholder="Confirm Password" name="confirm" required />
+                    </div>
+
+                    <button className="submit-auth" type="submit">Create Admin</button>
+                  </form>
                 )
 
             )
