be2d8936cad98605e7001234f7c9225d48f844d7
seasaltsaige <oriontothemax@gmail.com>
2024-05-08 15:56:35 -0700

    Work on login page, potential logos

--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -8,7 +8,7 @@ export function Login() {
   const signIn = useSignIn();
 
   // true: login, false: register, null: update
-  const [login_register_update, setLogin_Register_Update] = useState<boolean | null>(true);
+  const [login_register_update, setLogin_Register_Update] = useState<"login" | "register" | "update">("login");
 
   // signIn({
   //   auth: {
@@ -21,21 +21,52 @@ export function Login() {
   // })
 
   return (
-    login_register_update ?
-      (
-        // Login state
-        <></>
-      ) :
-      (
-        login_register_update === null ?
-          (
-            // Update state
-            <></>
-          ) :
-          (
-            // Register state
-            <></>
-          )
-      )
+
+    <div className="auth-form">
+      <div className="auth-type-list">
+        {
+          ["Login", "Register", "Update"].map((val, i) => (
+            <button
+              key={i}
+              className={`auth-button ${val.toLowerCase() === login_register_update ? "button-active" : ""}`}>
+              {val}
+            </button>
+          ))
+        }
+      </div>
+      <div className="auth-panel">
+        <img className="login-logo" src="/file.png" alt="Login Logo" />
+        {
+          login_register_update === "login" ?
+            (
+              // Login state
+
+              <form>
+                <div>
+                  <label htmlFor="username">Username</label>
+                  <input type="text" placeholder="Enter Username" name="username" required />
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input type="password" placeholder="Enter Password" name="password" required />
+                </div>
+
+                <button className="submit-auth" type="submit">Login</button>
+              </form>
+            ) :
+            (
+              login_register_update === "update" ?
+                (
+                  <></>
+                ) :
+                (
+                  <></>
+                )
+
+            )
+        }
+      </div>
+    </div>
   )
+
 }
