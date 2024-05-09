ed794e532dcb9c7fc37d71e5be3a669ba67085d1
seasaltsaige <oriontothemax@gmail.com>
2024-05-08 17:36:55 -0700

    Work on frontend login forms and api

--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useSignIn from "react-auth-kit/hooks/useSignIn";
 import "./Login.css";
+import checkAdmin from "../../api/checkAdmin";
 
 
 export function Login() {
@@ -10,6 +11,38 @@ export function Login() {
   // true: login, false: register, null: update
   const [login_register_update, setLogin_Register_Update] = useState<"login" | "register" | "update">("login");
 
+  const [loginObject, setLoginObject] = useState<{
+    username?: string;
+    password?: string;
+    newUsername?: string;
+    newPassword?: string;
+    confirmPassword?: string;
+  }>({});
+  
+  const clearForm = () => {
+    document.forms[0].reset();
+  }
+
+  const login = (ev: React.FormEvent<HTMLButtonElement>) => {
+    // ev.preventDefault();
+    console.log(loginObject);
+  }
+
+  const update = (ev: React.FormEvent<HTMLButtonElement>) => {
+    console.log(loginObject);
+  }
+
+
+  
+  const register = async (ev: React.FormEvent<HTMLButtonElement>) => {
+    const exists = await checkAdmin();
+    if (exists.status === 200) {
+      console.log("Good to go");
+    } else {
+      console.log("account exists");
+    }
+    console.log(loginObject);
+  }
   // signIn({
   //   auth: {
   //     token: "", // Token recieved from api
@@ -23,13 +56,14 @@ export function Login() {
   return (
 
     <div className="auth-form">
+
       <div className="auth-type-list">
         {
           ["Login", "Register", "Update"].map((val, i) => (
             <button
               key={i}
               className={`auth-button ${val.toLowerCase() === login_register_update ? "button-active" : ""}`}
-              onClick={() => setLogin_Register_Update(val.toLowerCase() as "login" | "register" | "update")}>
+              onClick={() => { setLoginObject({}); setLogin_Register_Update(val.toLowerCase() as "login" | "register" | "update"); clearForm() }}>
               {val}
             </button>
           ))
@@ -45,59 +79,59 @@ export function Login() {
               <form>
                 <div>
                   <label htmlFor="username">Username</label>
-                  <input type="text" placeholder="Enter Username" name="username" required />
+                  <input type="text" placeholder="Enter Username" name="username" required onChange={(ev) => setLoginObject((old) => ({ ...old, username: ev.target.value }))} />
                 </div>
                 <div>
                   <label htmlFor="password">Password</label>
-                  <input type="password" placeholder="Enter Password" name="password" required />
+                  <input type="password" placeholder="Enter Password" name="password" required onChange={(ev) => setLoginObject((old) => ({ ...old, password: ev.target.value }))} />
                 </div>
 
-                <button className="submit-auth" type="submit">Login</button>
+                <button onClick={(ev) => { ev.preventDefault(); login(ev) }} className="submit-auth" type="submit">Login</button>
               </form>
             ) :
             (
               login_register_update === "update" ?
                 (
-                  <form>
+                  <form id="update">
                     <div>
                       <label htmlFor="username">Username</label>
-                      <input type="text" placeholder="Enter Username" name="username" required />
+                      <input type="text" placeholder="Enter Username" name="username" required onChange={(ev) => setLoginObject((old) => ({ ...old, username: ev.target.value }))} />
                     </div>
                     <div>
                       <label htmlFor="password">Password</label>
-                      <input type="password" placeholder="Enter Password" name="password" required />
+                      <input type="password" placeholder="Enter Password" name="password" required onChange={(ev) => setLoginObject((old) => ({ ...old, password: ev.target.value }))} />
                     </div>
 
                     <div>
-                      <label style={{marginTop: 10, }} htmlFor="newUsername">New Username</label>
-                      <input type="text" placeholder="Enter New Username (optional)" name="newUsername" />
+                      <label style={{ marginTop: 10, }} htmlFor="newUsername">New Username</label>
+                      <input type="text" placeholder="Enter New Username (optional)" name="newUsername" onChange={(ev) => setLoginObject((old) => ({ ...old, newUsername: ev.target.value }))} />
                     </div>
                     <div>
                       <label htmlFor="newPassword">New Password</label>
-                      <input type="password" placeholder="Enter Password" name="newPassword" required />
+                      <input type="password" placeholder="Enter Password" name="newPassword" required onChange={(ev) => setLoginObject((old) => ({ ...old, newPassword: ev.target.value }))} />
                     </div>
 
 
-                    <button className="submit-auth" type="submit">Update Admin</button>
+                    <button onClick={(ev) => {ev.preventDefault(); update(ev); }} className="submit-auth" type="submit">Update Admin</button>
                   </form>
                 ) :
                 (
-                  <form>
+                  <form id="register">
                     <div>
                       <label htmlFor="username">Username</label>
-                      <input type="text" placeholder="Enter Username" name="username" required />
+                      <input type="text" placeholder="Enter Username" name="username" required onChange={(ev) => setLoginObject((old) => ({ ...old, username: ev.target.value }))}/>
                     </div>
                     <div>
                       <label htmlFor="password">Password</label>
-                      <input type="password" placeholder="Enter Password" name="password" required />
+                      <input type="password" placeholder="Enter Password" name="password" required onChange={(ev) => setLoginObject((old) => ({ ...old, password: ev.target.value }))} />
                     </div>
 
                     <div>
                       <label htmlFor="confirm">Confirm Password</label>
-                      <input type="password" placeholder="Confirm Password" name="confirm" required />
+                      <input type="password" placeholder="Confirm Password" name="confirm" required onChange={(ev) => setLoginObject((old) => ({ ...old, confirmPassword: ev.target.value }))}/>
                     </div>
 
-                    <button className="submit-auth" type="submit">Create Admin</button>
+                    <button onClick={(ev) => {ev.preventDefault(); register(ev); }} className="submit-auth" type="submit">Create Admin</button>
                   </form>
                 )
 
