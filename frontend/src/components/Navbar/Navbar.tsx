154acd5f9043d80b8b821a60165e4345df26d3bb
Maxisthemoose <oriontothemax@gmail.com>
2024-05-12 16:34:42 -0700

    dynamic navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import "./Navbar.css";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 function NavSpacer() {
   return (
     <div className="nav-spacer"></div>
@@ -12,31 +13,33 @@ function NavSpacer() {
 export default function () {
 
   const navigate = useNavigate();
-
+  const { height, width } = useWindowDimensions();
+  // console.log(width)
   return (
     <>
       <nav className="navbar">
         <div className="club-logo" onClick={() => navigate("/")}>
           <img className="club-logo-image" src="/SBCC_CSCLUB_LOGO_TRANSPARENT.png" alt="Club Logo" />
-          <p className="club-logo-text">SBCC Computer Science Club</p>
+          <p className="club-logo-text">{width > 950 ? "SBCC Computer Science Club" : "SBCC CS Club"}</p>
 
         </div>
         <div className="navbar-links">
-
-          <Link to="/about">About Us</Link>
-
-          {/* Drop Down link with officers, Present Past */}
-          <Link to="/officers/present">Present Officers</Link>
-          <Link to="/officers/past">Past Officers</Link>
-
-          {/* Drop Down link with events, Upcoming, Past */}
-          <Link to="/events/upcoming">Upcoming Events</Link>
-          <Link to="/events/past">Past Events</Link>
-
-
-          <Link to="/elections">Elections</Link>
-          {/* <Link to="/about">About Us</Link> */}
-          {/* <Link to="/about">About Us</Link> */}
+          {
+            width > 1025 ?
+              <>
+                <Link to="/about">About Us</Link>
+                {/* Drop Down link with officers, Present Past */}
+                <Link to="/officers/present">Present Officers</Link>
+                <Link to="/officers/past">Past Officers</Link>
+                {/* Drop Down link with events, Upcoming, Past */}
+                <Link to="/events/upcoming">Upcoming Events</Link>
+                <Link to="/events/past">Past Events</Link>
+                <Link to="/elections">Elections</Link>
+                {/* <Link to="/about">About Us</Link> */}
+                {/* <Link to="/about">About Us</Link> */}
+              </>
+              : <>HAMBURGER !!!!</>
+          }
         </div>
       </nav>
       <NavSpacer />
