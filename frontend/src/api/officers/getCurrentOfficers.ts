9f7fa1c9f9de40b40ed4a4ad9a92959f03cfd292
Maxisthemoose <oriontothemax@gmail.com>
2024-05-20 16:24:50 -0700

    create event api files

--- frontend/src/api/officers/getCurrentOfficers.ts
+++ frontend/src/api/officers/getCurrentOfficers.ts
@@ -3,7 +3,5 @@ import axios from "axios";
 const URL = process.env.REACT_APP_URL;
 
 export async function getCurrentOfficers(auth?: string) {
-  console.log(URL, "in req");
-
   return await axios.get(`${URL}/officers/current`, { headers: { Authorization: auth } });
 }
