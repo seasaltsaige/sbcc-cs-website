dcd7e2118e5bc22178fc71156578ad4e63659403
Maxisthemoose <oriontothemax@gmail.com>
2024-05-25 16:34:58 -0700

    seperate page for officers to create/update/delete candidates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About, CurrentOfficers, ElectionsHome, Home, PastEvents, PastOfficers, UpcomingEvents, Vote, Login } from "./routes";
+import { About, CurrentOfficers, ElectionsHome, Home, PastEvents, PastOfficers, UpcomingEvents, Vote, Login, AllCandidates } from "./routes";
+import { RequireAuth } from 'react-auth-kit';
 
 
 function App() {
@@ -17,6 +18,11 @@ function App() {
         <Route path="/events/past" element={<PastEvents />} />
         <Route path="/elections" element={<ElectionsHome />} />
         <Route path="/elections/vote" element={<Vote />} />
+        <Route path="/elections/candidates" element={
+          <RequireAuth loginPath="/elections">
+            <AllCandidates />
+          </RequireAuth>
+        } />
 
         <Route path="/admin/login" element={<Login />} />
       </Routes>
---
+++ frontend/src/routes/Elections/AllCandidates/AllCandidates.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./AllCandidates.css";
+import { Navbar } from "../../../components";
+
+export function AllCandidates() {
+  return (
+    <>
+      <Navbar />
+      Requires Auth!
+    </>
+  )
+}
