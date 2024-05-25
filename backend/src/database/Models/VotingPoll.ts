4c6d323017be25868ad15540156129918425b91e
Maxisthemoose <oriontothemax@gmail.com>
2024-05-25 15:55:35 -0700

    change voting poll schema

--- backend/src/database/Models/VotingPoll.ts
+++ backend/src/database/Models/VotingPoll.ts
@@ -3,7 +3,14 @@ import { Schema, model } from "mongoose";
 // A poll that organizes a vote
 // will contain references to all the candidates associated with the poll
 const VotingPoll = new Schema({
-  candidates: [Schema.Types.ObjectId],
+
+  presidents: [Schema.Types.ObjectId],
+  vicepresidents: [Schema.Types.ObjectId],
+  projectmanager: [Schema.Types.ObjectId],
+  secretarys: [Schema.Types.ObjectId],
+  tresurers: [Schema.Types.ObjectId],
+  promoters: [Schema.Types.ObjectId],
+
   // Range of times to vote for polling
   voteTime: {
     start: Date,
